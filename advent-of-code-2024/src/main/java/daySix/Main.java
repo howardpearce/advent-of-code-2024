adf2f470b686c9e443931f5a643ee355ad1bdc27
Howard Pearce <howard.pearce@arcticwolf.com>
2024-12-08 11:16:03 -0400

    day six

--- advent-of-code-2024/src/main/java/daySix/Main.java
+++ advent-of-code-2024/src/main/java/daySix/Main.java
@@ -13,32 +13,45 @@ public class Main {
   static Logger logger;
 
   static Map<Point, Character> map = new HashMap<>();
-  static List<Point> visitedSpots = new ArrayList<>();
-  static Guard guard;
-  static char[][] grid;
 
-  public record Point(int x, int y) {
-    @Override
-    public String toString() {
-      return String.format("(%d, %d)", x, y);
-    }
+  // New exception type for when the guard is looping
+  public static class LoopingException extends Exception {
+    public LoopingException(String message) { super(message); }
   }
+
+  public record Point(int x, int y) {}
+
+  public record Move(Point position, Direction direction) {}
 
   // maybe this could be a record
   public static class Guard {
     public Point position;
     public Direction direction;
-    public Guard(Point position, Direction direction) {
+    public List<Point> visitedSpots = new ArrayList<>();
+    public List<Move> moves = new ArrayList<>();
+    public Map<Point, Character> map;
+    boolean looping = false;
+
+    public Guard(Point position, Direction direction, Map<Point, Character> map) {
       this.position = position;
       this.direction = direction;
+      this.map = map;
     }
 
-    public void move() throws ArrayIndexOutOfBoundsException{
+    public Guard(Guard guard) {
+      this.position = new Point(guard.position.x, guard.position.y);
+      this.direction = guard.direction;
+      this.visitedSpots = new ArrayList<>(guard.visitedSpots);
+      this.moves = new ArrayList<>(guard.moves);
+      this.map = new HashMap<>(guard.map);
+    }
+
+    public void move() throws ArrayIndexOutOfBoundsException, LoopingException {
       Point next = new Point(position.x + direction.xDir, position.y + direction.yDir);
-      if (!map.containsKey(next)) {
+      if (!this.map.containsKey(next)) {
         throw new ArrayIndexOutOfBoundsException("Guard is out of bounds at point: " + next.toString());
       }
-      if (map.get(next) == '#') {
+      if (this.map.get(next) == '#') {
         // turn right
         switch (direction) {
           case UP:
@@ -56,6 +69,11 @@ public class Main {
         }
       } else {
         visitedSpots.add(next);
+        Move move = new Move(next, direction);
+        if (moves.contains(move)) {
+          throw new LoopingException("Guard is looping at point: " + next.toString());
+        }
+        moves.add(new Move(next, direction));
         position = next;
       }
     }
@@ -74,57 +92,77 @@ public class Main {
     }
   }
 
+  public static boolean checkIfLooping(Guard guard) {
+    try {
+      while (true) {
+        guard.move();
+      }
+    } catch(ArrayIndexOutOfBoundsException e) {
+      return false;
+    } catch (LoopingException e) {
+      return true;
+    }
+  }
+
   public static void main (String[] args) throws IOException {
     logger = utils.utilities.getLogger(daySix.Main.class);
     List<String> input = utilities.getInput(System.getProperty("user.dir") + "/src/main/resources/daySix.txt");
 
     // for visualizing the grid
-    grid = new char[input.size()][input.get(0).length()];
+    Move origin = new Move(new Point(0, 0), Direction.UP);
 
     for(int y = 0; y < input.size(); y++) {
       String line = input.get(y);
       for (int x = 0; x < line.length(); x++) {
         if (line.charAt(x) == '^') {
-          guard = new Guard(new Point(x, y), Direction.UP);
+          origin = new Move(new Point(x, y), Direction.UP);
         }
         map.put(new Point(x, y), line.charAt(x));
       }
     }
-
-    logger.info("Guard position: {}", map.get(new Point(4, 6)));
-    logger.info("Part one: {}", partOne());
+    try {
+      logger.info("Part one: {}", partOne(origin));
+      logger.info("Part two: {}", partTwo(origin));
+    } catch (LoopingException e) {
+      logger.debug("Guard is looping: {}", e.getMessage());
+    }
   }
 
-  public static int partOne() {
+  public static int partOne(Move origin) throws LoopingException {
+    Guard guard = new Guard(origin.position, origin.direction, map);
     try {
       while (true) {
         guard.move();
       }
     } catch (ArrayIndexOutOfBoundsException e) {
-      logger.debug(e.toString());
+      logger.debug("Guard reached the end of the map: {}", guard.position.toString());
     }
 
-    for (Point p : map.keySet()) {
-      grid[p.y][p.x] = map.get(p);
-    }
-
-    for(Point p : visitedSpots) {
-      grid[p.y][p.x] = 'X';
-    }
-
-    // print the grid
-    for(int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[i].length; j++) {
-        System.out.print(grid[i][j]);
-      }
-      System.out.print("\n");
-    }
-
-    return visitedSpots.stream().distinct().toList().size();
+    return guard.visitedSpots.stream().distinct().toList().size();
   }
 
-  public static int partTwo() {
-    return 0;
+  public static int partTwo(Move origin) throws LoopingException {
+    Guard guard = new Guard(origin.position, origin.direction, map);
+    List<Point> obstacles = new ArrayList<>();
+    try {
+      while (true) {
+        Point next = new Point(guard.position.x + guard.direction.xDir, guard.position.y + guard.direction.yDir);
+        if (guard.map.containsKey(next) && guard.map.get(next) != '#') {
+          if (!guard.visitedSpots.contains(next)) {
+            Guard newGuard = new Guard(guard);
+            newGuard.map.put(next, '#');
+            if (checkIfLooping(newGuard)) {
+              obstacles.add(next);
+            }
+          }
+        }
+        guard.move();
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      // do nothing
+    }
+
+    return obstacles.stream().distinct().toList().size();
   }
 
 }
